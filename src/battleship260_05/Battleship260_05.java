b02204f82c5e865dbd9a80757df9859e0071f0ed
Logan Work <Logan Work@LoganWork-PC>
2015-01-21 21:34:26 -0800

    Group Assignment 1 Commit!

--- src/battleship260_05/Battleship260_05.java
+++ src/battleship260_05/Battleship260_05.java
@@ -9,19 +9,54 @@ package battleship260_05;
  *
  * @author Logan Work
  */
+
+
 public class Battleship260_05 {
 
     /**
      * @param args the command line arguments
      */
+    String player1 = "Joe";
+    String player2 = "Lucy";
+    
+    void showPlayerOne() {
+        System.out.println(player1);
+    }
+    void showPlayerTwo() {
+        System.out.println(player2);
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
+                
+        Battleship260_05 newGame = new Battleship260_05();
         
+        newGame.showPlayerOne();
+        
+        newGame.showPlayerTwo();
+        
+        Board setupBoard = new Board();
+        
+        setupBoard.showBoardSize();
+        
+        setupBoard.showBoardType();
+        
+       
         //Alex Griffiths was here testing this out
         //Christian Molina, testing through NetBeans as well
         //Rachel Yancey here and tested
-        
-       //Wendell Tanner - Test
+        //Wendell Tanner - Test
     }
     
+}
+class Players {
+    /*String player1 = "Joe";
+    String player2 = "Lucy";
+    
+    void showPlayerOne() {
+        System.out.println(player1);
+    }
+    void showPlayerTwo() {
+        System.out.println(player2);
+    }*/
 }
