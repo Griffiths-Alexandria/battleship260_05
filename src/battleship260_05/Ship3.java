72fcb0959672204bfffd466adac453798e861fc5
Molina-Christian <mol10004@byui.edu>
2015-01-22 18:44:57 -0700

    Christian Molina, adding Ships

---
+++ src/battleship260_05/Ship3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship260_05;
+
+/**
+ *
+ * @author Christian
+ */
+public class Ship3 {
+    String description = "The First 4-Space Ship";
+    int size = 4;
+    int hitCount = 4;
+    
+    void showDescription() {
+        System.out.println(description);
+    }
+    void showSize() {
+        System.out.println("Takes up " + size + " spaces.");
+    }
+    void showHitCount() {
+        System.out.println("Can take " + hitCount + " hits.");
+    }
+}
