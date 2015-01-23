f26a75e6ca746514c38eefc0cea718eb3c36617f
Molina-Christian <mol10004@byui.edu>
2015-01-22 18:44:33 -0700

    Christian Molina, adding Ships

--- src/battleship260_05/Battleship260_05.java
+++ src/battleship260_05/Battleship260_05.java
@@ -47,6 +47,34 @@ public class Battleship260_05 {
 
 //is this what it means on the last bullet point of the individual assignment?
         
+        // Creating new instances of each ship
+        Ship1 newShip1 = new Ship1();
+        Ship2 newShip2 = new Ship2();
+        Ship3 newShip3 = new Ship3();
+        Ship4 newShip4 = new Ship4();
+        Ship5 newShip5 = new Ship5();
+        
+        //Displaying each ship info
+        newShip1.showDescription();
+        newShip1.showSize();
+        newShip1.showHitCount();
+        
+        newShip2.showDescription();
+        newShip2.showSize();
+        newShip2.showHitCount();
+        
+        newShip3.showDescription();
+        newShip3.showSize();
+        newShip3.showHitCount();
+        
+        newShip4.showDescription();
+        newShip4.showSize();
+        newShip4.showHitCount();
+        
+        newShip5.showDescription();
+        newShip4.showSize();
+        newShip5.showHitCount();
+        
         //Sue Francis testing
         //Alex Griffiths was here testing this out
         //Christian Molina, testing through NetBeans as well
