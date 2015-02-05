17c850bbd1e956d9c1d34c610f82c99b2d828679
Molina-Christian <mol10004@byui.edu>
2015-02-04 20:01:14 -0700

    Editing PlayerMenuView

--- src/battleship260_05/PlayerMenuView.java
+++ src/battleship260_05/PlayerMenuView.java
@@ -15,49 +15,50 @@ public class PlayerMenuView {
         {"2.", "PLAYER VS COMPUTER"},
         {"3.", "EXIT"}
         };
-    }
     PlayerMenuControl playerMenuControl = new PlayerMenuControl();
 
-public PlayerMenuView() {
+    public PlayerMenuView() {
+
+    }
     
-}
-public void getInput(){
-        
-        String command;
-        Scanner inFile = new Scanner(System.in);
-        
-        do {
-            this.display();
-            
-            command = inFile.nextLine();
-            command = command.trim().toUpperCase();
-            
-            switch (command) {
-                case "1"://PLAYER VS PLAYER
-                    this.playerMenuControl.startTwoPlayerGame();
-                    break;
-                case "2"://PLAYER VS COMPUTER
-                    this.playerMenuControl.startOnePlayerGame();
-                    break;
-                case "3"://EXIT
-                    this.playerMenuControl.returntoPreviousMenu();
-                    break;
-                default: 
-                    new BattleshipError().displayError("Invalid command. Please enter a valid command.");
-                    //continue;    
-            }
-        }while(!command.equals("3"));
-}
-public final void display(){
-            System.out.println("\n**********************************************************************");
-            System.out.println("**                         PLAYER  MENU                            **");
-            System.out.println("**********************************************************************\n");
-            
-            for (int i = 0;i < PlayerMenuView.menuItems.length;i++){
-                System.out.println("\t" + menuItems[i][0] + " " + menuItems[i][1] + "\n");
-                
-            }
-            System.out.println();
-            System.out.print("Please Enter Your Choice: ");
+    public void getInput(){
+
+            String command;
+            Scanner inFile = new Scanner(System.in);
+
+            do {
+                this.display();
+
+                command = inFile.nextLine();
+                command = command.trim().toUpperCase();
+
+                switch (command) {
+                    case "1"://PLAYER VS PLAYER
+                        this.playerMenuControl.startTwoPlayerGame();
+                        break;
+                    case "2"://PLAYER VS COMPUTER
+                        this.playerMenuControl.startOnePlayerGame();
+                        break;
+                    case "3"://EXIT
+                        this.playerMenuControl.returntoPreviousMenu();
+                        break;
+                    default: 
+                        new BattleshipError().displayError("Invalid command. Please enter a valid command.");
+                        //continue;    
+                }
+            }while(!command.equals("3"));
+    }
+    
+    public final void display(){
+                System.out.println("\n**********************************************************************");
+                System.out.println("**                          PLAYER  MENU                            **");
+                System.out.println("**********************************************************************\n");
+
+                for (int i = 0;i < PlayerMenuView.menuItems.length;i++){
+                    System.out.println("\t" + menuItems[i][0] + " " + menuItems[i][1] + "\n");
+
+                }
+                System.out.println();
+                System.out.print("Please Enter Your Choice: ");
     }
 }
