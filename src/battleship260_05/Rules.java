708c6361edd93f8267f46ea10dfff08cd867aae9
Griffiths-Alexandria <alexcgriff@gmail.com>
2015-02-04 21:04:42 -0800

    Changes in my individual Rules Class to update

--- src/battleship260_05/Rules.java
+++ src/battleship260_05/Rules.java
@@ -5,7 +5,7 @@
  */
 package battleship260_05;
 
-import java.text.DecimalFormat;
+//import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /**
@@ -16,24 +16,22 @@ public class Rules {
     
     //Individual Assignment 3 Alex Griffiths
     double pageselect;
-      double nextpage;
-       
-      
-    
+    double nextpage;
+        
     void getNextPage(double pageselect, double nextpage) {
         if (pageselect >= 8 && pageselect < 0) {
             System.out.println("Invalid Page Selection\n");
-            return;
+          return;
         }
         if (1 < nextpage) {
             System.out.println("Please Select 1 to Continue\n");
-            return;
+           return;
         }
         
         double pagecalc = (nextpage - 1); 
-        int determinepage = (int)(pageselect)+ (int)(pagecalc);
+      int determinepage = (int)(pageselect)+ (int)(pagecalc);
         
-        System.out.println("We will direct you to page: \t" + determinepage + "\n");
+       System.out.println("We will direct you to page: \t" + determinepage + "\n");
     }
     //End Individual Assignment 3 
     
@@ -43,51 +41,88 @@ public class Rules {
             +"Good Luck! \n\n"; 
       
 //experimenting with page and Y/N if/then loops
-    String pageone = "PREPARE FOR BATTLE\n\n"
+public void displayPrepareBattle() {
+    System.out.println();
+    this.displayRulesBorder();
+    System.out.println(
+   
+    "\tPREPARE FOR BATTLE\n\n"
         +" Each player will place their fleet of 5 ships on your ocean \n" 
         +"grid. Each ship can be placed in any horizontal or vertical \n" 
         +"position. All ships must be placed within the boundaries of\n" 
         +"the grid, and no two ships can occupy the same gridspace. Once\n" 
         +"the ships have been set they cannot be moved and the game will\n" 
-        +"begin.\n\n"; 
+        +"begin.\n\n"); 
     
-     String pagetwo= "HOW TO PLAY\n\n"
+}  
+
+public void displayHowToPlay() {
+    System.out.println(); 
+    this.displayRulesBorder();
+    System.out.println("\t HOW TO PLAY\n\n"
         +"Choose which partner will go first. Turns will\n" 
         +"alternate with each player calling out one shot per turn to\n"
-        +"try to hit each other’s ships.\n"; 
+        +"try to hit each other’s ships.\n"); 
+} 
     
-    String pagethree = "CALL YOUR SHOT!\n\n"
+    
+    
+public void displayCallShots() {
+    System.out.println();
+    this.displayRulesBorder(); 
+    System.out.println("CALL YOUR SHOT!\n\n"
         +" On your turn, enter a target coordinate by typing the\n" 
-        +"horizontal and vertical coordinate values."; 
+        +"horizontal and vertical coordinate values.");
+}
     
-    String pagefour = "IT’S A HIT!\n\n"
+public void displayItsAHit(){
+    System.out.println(); 
+    this.displayRulesBorder();
+    System.out.println("\tIT’S A HIT!\n\n"
         +" If your target location is occupied by an enemy ship, the \n" 
         +"shot is a hit. You will be notified that a hit has been made,\n" 
         +"as well as which ship was hit. The hit will be recorded on \n" 
-        +"your opponent’s grid by a ‘X’."; 
+        +"your opponent’s grid by a ‘X’.");
+}
     
-    String pagefive = "IT’S A MISS!\n\n"
+public void displayItsAMiss(){
+    System.out.println(); 
+    this.displayRulesBorder();
+    System.out.println("\t IT’S A MISS!\n\n"
         +" If your target location is not occupied by an enemy ship, it \n" 
         +"is a miss. You will be notified that the shot missed and it \n" 
-        +"will be recorded on the opponent’s grid by an ‘O’."; 
+        +"will be recorded on the opponent’s grid by an ‘O’.");
+}
     
-    String pagesix = "SINKING A SHIP\n\n" 
+public void displaySinkingShipRule(){
+    System.out.println(); 
+    this.displayRulesBorder();
+    System.out.println("\t SINKING A SHIP\n\n" 
         +"Once all of one ship’s occupied grid spaces have been hit, the \n"
         +"ship has been sunk. You will be notified which ship was sunk \n" 
-        +"and the grid will convert all hit locations from ‘X’ to ‘#’."; 
+        +"and the grid will convert all hit locations from ‘X’ to ‘#’."); 
+} 
     
-    String pageseven = "WINNING THE GAME\n\n" 
-        +"The first player to sink all 5 of the opponent’s ship wins the\n" 
-        +"game. Just try not to sprain anything with your victory dance."; 
+public void displayWinningGame() {
+    System.out.println();
+    this.displayRulesBorder();
+    System.out.println("\tWINNING THE GAME\n\n"
+       +"The first player to sink all 5 of the opponent’s ship wins the \n" 
+       +"game. Just try not to sprain anything with your victory dance. \n"); 
+}
     
-    String pagesalvo = "SALVO MODE\n\n" 
+public void displaySalvoModeRules() {
+    System.out.println(); 
+    this.displayRulesBorder(); 
+    System.out.println("\tSALVO MODE\n\n"
         + "Salvo Mode uses the basic rules of the game, except each \n" 
-        +"player fires as many shots as they have ships in play each\n" 
-        +"turn. For example, at the start of play, the first player \n" 
-        +"gives 5 sets of coordinates(one for each ship), after which he\n" 
-        +"is notified of the hits and misses. When one of his ship’s is \n" 
+       +"player fires as many shots as they have ships in play each\n" 
+       +"turn. For example, at the start of play, the first player \n" 
+       +"gives 5 sets of coordinates(one for each ship), after which he\n" 
+       +"is notified of the hits and misses. When one of his ship’s is\n" 
         +"sank, he will only call 4 shots on his next turn. The more of\n" 
-        +"your ships sink, the less shots you get."; 
+       +"your ships sink, the less shots you get.\n"); 
+}
     
   /* Individual Assignment Week 2 
         public void displayRules() {
@@ -95,11 +130,18 @@ public class Rules {
         System.out.println(this.instructions); 
     }
     */
+    public void displayRulesBorder(){
+        System.out.println(
+        "\t*********************************************************"); 
+    }
     
-    
-    
+    public void goBackToMain(){
+        System.out.println(); 
+        this.displayRulesBorder();
+        System.out.println("This will send you back to the Main Menu"); 
+    }
     public String getRules() {
-        return pageone;
+        return instructions;
        
     }
     
--- src/battleship260_05/RulesView.java
+++ src/battleship260_05/RulesView.java
@@ -5,10 +5,103 @@
  */
 package battleship260_05;
 
+import java.util.Scanner;
+
 /**
  *
  * @author griffiths
  */
 public class RulesView {
     
+    private Rules Rules;   
+    
+    
+    private final static String[][] RulesItems = {
+        {"1.", "PREPARE FOR BATTLE"},
+        {"2.", "HOW TO PLAY"},
+        {"3.", "CALL YOUR SHOT"}, 
+        {"4.", "IT'S A HIT"},
+        {"5.", "IT'S A MISS"}, 
+        {"6.", "SINKING A SHIP"}, 
+        {"7.", "WINNING THE GAME"}, 
+        {"8.", "SALVO MODE"},
+        {"9.", "GO BACK TO MAIN MENU"}, 
+        
+         };
+   
+    // Create instance of the Rules (action) class
+    private Rules rules = new Rules(); 
+
+    //default constructor
+    public RulesView(){
+    
+    }
+    
+    //display the rules menu and get the users input selection
+    public void getInput(){
+        
+        String command;
+        Scanner inFile = new Scanner(System.in);
+        
+        do {
+            this.display();
+            
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            
+            switch (command) {
+                
+                case "1"://Prepare Battle Scene
+                    this.Rules.displayPrepareBattle();
+                    break;
+                case "2"://How To Play
+                    this.Rules.displayHowToPlay();
+                    break;
+                case "3"://Calling Shots
+                    this.Rules.displayCallShots();
+                    break;
+                case "4"://Hit Rule
+                    this.Rules.displayItsAHit();
+                    break;
+                case "5"://Miss Rule
+                    this.Rules.displayItsAMiss();
+                    break;
+                case "6"://Sinking Ship Rule
+                    this.Rules.displaySinkingShipRule();
+                    break;
+                case "7"://Winning Game Rules
+                    this.Rules.displayWinningGame();
+                    break;
+                case "8"://Salvo Rules
+                    this.Rules.displaySalvoModeRules();
+                    break;
+                case "9"://go back to main
+                    this.Rules.goBackToMain();
+                    break;
+                case "10":
+                    break;
+                default: 
+                    new BattleshipError().displayError("Invalid command. Please enter a valid command.");
+                    continue;    
+            }
+        } while(!command.equals("10"));
+        
+        return; 
+    }
+    
+    public final void display(){
+            System.out.println("\n**********************************************************************");
+            System.out.println("\t**                           RULES MENU                             **");
+            System.out.println("\t**********************************************************************\n");
+            System.out.println(this.Rules.pageone)
+            System.out.print("\n\nPlease Enter Your Choice: ");
+            
+            for (int i = 0;i < RulesView.RulesItems.length;i++){
+                System.out.println("\t" + RulesItems[i][0] + " " + RulesItems[i][1] + "\n");
+                
+            }
+            System.out.println("**********************************************************************\n");
+    }
+}
+
 }
