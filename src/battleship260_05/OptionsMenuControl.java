63b5844e30a6b73b49b3241961216cd4821e5114
Chio's computer <Chio's computer@D18R79C1>
2015-02-07 12:09:07 -0600

    Daniel Diaz Assignment: OptionsMenuControl, ready.

--- src/battleship260_05/OptionsMenuControl.java
+++ src/battleship260_05/OptionsMenuControl.java
@@ -8,7 +8,34 @@ package battleship260_05;
 /**
  *
  * @author Daniel Diaz
+ * //I went simple on this coding so we do not get complicated Sterling.
+ * //It is ready for your OptionsMenuView, Happy coding man!
  */
 public class OptionsMenuControl {
     
+    //I think we will have to add a subclass to get theese other options working.
+    public void displayGridSize(){
+        System.out.println("SMALL             MEDIUM                LARGE");
+    }
+    
+    public void displayNumberOfShips(){
+        System.out.println("LOW               MEDIUM                HIGH");
+    }
+    
+    public void displayShipIdentification(){
+        System.out.println("         ON                     OFF");
+    }
+    
+    public void displaySalvoMode(){
+        System.out.println("         ON                     OFF");
+    }
+    
+    public void displaySaveSettings(){
+        System.out.println("        SAVE                   CANCEL");
+    }
+    
+    public void displayMainMenu(){
+        MainMenuView mainMenu = new MainMenuView();
+        mainMenu.getInput(); 
+    }
 }
