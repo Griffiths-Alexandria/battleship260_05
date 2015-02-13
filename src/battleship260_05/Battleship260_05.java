a59820a7c8405ee9fbf596eb66f8b3cccb78a36b
ryancey <rachel14yancey@gmail.com>
2015-02-12 23:41:42 -0800

    Individual programming assignment 5

--- src/battleship260_05/Battleship260_05.java
+++ src/battleship260_05/Battleship260_05.java
@@ -43,13 +43,16 @@ public class Battleship260_05 {
     // MAIN METHOD
     public static void main(String[] args) {
         
+        //rachel's individual assignment 5
+        Turn tinput = new Turn();
+       tinput.showsalvoShots();
         
-        //Paired programming assignment 5
+//Paired programming assignment 5
         BoardView testinput = new BoardView();
         String response = testinput.getInput();
         System.out.println(response);
         
-        
+       
         
         
         
--- src/battleship260_05/Turn.java
+++ src/battleship260_05/Turn.java
@@ -15,9 +15,36 @@ public class Turn {
   public void showcurrentPlayer(){
     System.out.println(currentPlayer);
  } 
- 
+ public void showsalvoShots(){
+  int shipSunk[] = new int[6];
+    //this is an array of possible ships sunk
+    shipSunk[0] = 0;
+    shipSunk[1] = 1;
+    shipSunk[2] = 2;
+    shipSunk[3] = 3;
+    shipSunk[4] = 4;
+    shipSunk[5] = 5;
+    
+    int i;
+    int Calculateshotsleft;
+    int numshipSunk = 3;//if this were to be used this is where the user input 
+    for(i = 0; i < shipSunk.length;i++){
+   Calculateshotsleft = 5 - shipSunk[i];//this is substracting 5 and the values
+   if (numshipSunk == i){
+    System.out.println("You have "+ Calculateshotsleft+ " shot(s) left.");
+   } 
+   //there are a couple things I realize I could add to make this code better like check the users input and display an error, however we already have a function that does this. For the purpose of the individual assignment, I kept it really basic as this isn't what we will use. 
+  
+    } 
+ }
+  
+  
+  
+  
+  
+  
   //This function is to be used at the expert(Salvo) level. It calculates how much Salvo shots are left. It will output numerically as a whole number and a percentage.
-  int shipSunk;
+  /*int shipSunk;
    int TotaLships;
   public static void getsalvoshots(int shipSunk, int TotaLships){
 //before calculating how many shots are left, the function will check the number. Typically in Salvo there are only 5 ships, however  I left it open for other programmers to decide to change the total number
@@ -34,7 +61,7 @@ int calculateshotsleft = (TotaLships - shipSunk);
 int salvoShots = (int) (percentofshots * 100);//casting double to int since we are dealing with whole numbers in percent
 System.out.println(salvoShots + "% of shots left or \n" + calculateshotsleft +" \b shots left. " );
 }
- }
+ }*/
 }
 
 
