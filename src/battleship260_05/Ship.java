a4f2fae0aa3340da05c2e3d89aac004755d721f6
Fred and Karen Call <Fred and Karen Call@FredandKarenCal>
2015-02-26 09:48:30 -0700

    updating Ship class with coOrd array... Christian

--- src/battleship260_05/Ship.java
+++ src/battleship260_05/Ship.java
@@ -14,39 +14,30 @@ public class Ship {
     String name;
     String description;
     int size;
+    int[][] coOrd;
     Boolean sunk = false;
     
     Ship(String name, String description, int size) {
         this.name = name;
         this.description = description;
         this.size = size;
+        this.coOrd = new int[this.size][2];
     }
     
     void getInfo() {
         System.out.println(this.name + ", " + this.description + "\n" + "It has a Hit Count of " + this.size);
     }
     
-    //Stuff for Individual Assignment
-    Random coin = new Random(); //The random operator works great! I will try to use it on my class next time, cool.
-    int result;
-    
-    void hitReduction() {
-        result = coin.nextInt(100);
-        if (result >= 50) {
-            if (size == 0) {
-                System.out.println("There is no ship remaining!");
-            }
-            else {
-                size -= 1;
-                System.out.println("A Ship has been hit!");
-                if (size <= 0) {
-                    sunk = true;
-                    System.out.println("Ship Destroyed!");
-                }
-            }
-        }
-        else {
-            System.out.println("You missed!");
-        }
+    public int hitReduction() {
+        // if coOrd is chosen
+            // if the space has already been marked
+                // return -1
+            // otherwise
+                // reduce size by 1
+                // if size equals 0
+                    // set the boolean sunk equal to true
+                    // return 2
+                // return 1
+        return 0;
     }
 }
